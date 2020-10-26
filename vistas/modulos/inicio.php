c83eb0653228f732fedc71c5068ad83ec5a6ab48
Jonathan Arias <informesarias@gmail.com>
2020-10-26 18:31:34 -0500

    se creó pagina mostrar producto o stock

--- vistas/modulos/inicio.php
+++ vistas/modulos/inicio.php
@@ -1,7 +1,7 @@
 
-<div class="row">
+
     <div class="col-6" id="contenidoInicio">
-        <div class="row" id="logo">
+        <div class="row p-3" id="logo">
             <div class="col-12">
                 <h1 id="nombreLogo">MaxiKomputo</h1>                    
             </div>
@@ -12,21 +12,32 @@
         <div class="row">
             <div class="col-12 d-flex">
                 <div class="mr-2">
-                    <a href="registro"><span class="btn btn-primary">Registrar Producto</span></a>
+                    <a href="registro" class="btn btn-primary ml-2">
+                        <span class="mr-2">
+                        <i class="fas fa-database"></i>
+                        </span>
+                        Registrar Producto
+                    </a>
                 </div>
                 <div class="ml-2">
-                <a href="stock" class="btn btn-primary">Stock Productos</a>
+                    <a href="stock" class="btn btn-primary ml-2">
+                        <span class="mr-2">
+                        <i class="fas fa-store"></i>
+                        </span>
+                        Stock Productos
+                    </a>
                 </div>
             </div>
         </div>
     </div>
 
     <div class="col-6 d-flex justify-content-center" id="imgInicio">
-        <div>
-            <img src="https://i.postimg.cc/SNGB5Fvf/Recurso-2.png" class="img-fluid">
+    
+        <div class="row">
+            <div>
+                <img src="https://i.postimg.cc/SNGB5Fvf/Recurso-2.png" class="img-fluid">
+            </div>
         </div>
     </div>
-
-</div>
        
 
