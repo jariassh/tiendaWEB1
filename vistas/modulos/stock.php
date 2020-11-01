3e6edca4467c7438d462625e04e067ffe79d9562
Jonathan Arias <informesarias@gmail.com>
2020-10-31 19:57:21 -0500

    Se creó ventana modal y se anima cards con efecto hover

--- vistas/modulos/stock.php
+++ vistas/modulos/stock.php
@@ -27,23 +27,24 @@
         </header>
     </div>
 </div>
-<div class="container ">
+<div class="container">
     <div class="row mt-2 mb-4">
         <div class="card col-5th border-0 bg-transparent">
             <a href="#">
                 <img class="card-img-top img-fluid" src="https://i.postimg.cc/5jcC62cD/Portatil-2.jpg" alt="Card image">
             </a>
             <div class="card-body bg-light border shadow-sm text-center p-2">
-                <h6 class="card-title text-center">
+                <h4 class="card-title text-center">
                    <strong>$ 4.199.000 COP</strong>
-                </h6>
+                </h4>
                 <div class="clearfix text-center">
                     <kbd class="">Descripci&oacute;n</kbd>
                 </div>
                 <p class="card-text text-justify small mt-2">Coputador Portatil ASUS VivoBook X413FA-BV545T, Sistema Operativo: Windows 10 Home - 64bit, Pantalla 14" Pulgadas, SSD 512GB, Procesador Intel Core i5 10210U Processor 1.6 GHz, RAM 8 GB DDR3L, Garantía 1 Año.</p>
             </div>
             <div class="card-footer bg-light text-center">
-                <a href="#" class="btn btn-primary btn-sm my-1">Ver Producto</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalProducto">Ver Producto</button>
+                <!-- <a href="#" class="btn btn-primary btn-sm my-1">Ver Producto</a> -->
             </div>
         </div>
 
@@ -52,16 +53,16 @@
                 <img class="card-img-top img-fluid" src="https://i.postimg.cc/cLhPMKD7/Portatil-3.jpg" alt="Card image">
             </a>
             <div class="card-body bg-light border shadow-sm p-2">
-                <h6 class="card-title text-center">
+                <h4 class="card-title text-center">
                    <strong>$ 5.699.000 COP</strong>
-                </h6>
+                </h4>
                 <div class="clearfix text-center">
                     <kbd class="">Descripci&oacute;n</kbd>
                 </div>
                 <p class="card-text text-justify small mt-2">Computador Portatil Gamer Legion 5, Sistema Operativo Windows 10 HOME, Disco Híbrido 1TB HDD + SSD 128GB, Pantalla 15.6" Pulgadas, Procesador Intel Core I5 10300H 2.5G, RAM 16GB DDR4, Garantía 12 Meses</p>
             </div>
             <div class="card-footer bg-light text-center">
-                <a href="#" class="btn btn-primary btn-sm my-1">Ver Producto</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalProducto">Ver Producto</button>
             </div>
         </div>
         
@@ -70,16 +71,16 @@
                 <img class="card-img-top img-fluid" src="https://i.postimg.cc/nr93yrYW/Portatil-5.png" alt="Card image">
             </a>
             <div class="card-body bg-light border shadow-sm p-2">
-                <h6 class="card-title text-center">
+                <h4 class="card-title text-center">
                    <strong>$ 2.399.000 COP</strong>
-                </h6>
+                </h4>
                 <div class="clearfix text-center">
                     <kbd class="">Descripci&oacute;n</kbd>
                 </div>
                 <p class="card-text text-justify small mt-2">HP Envy 13-AQ1001LA, Sistema Operativo Windows 10 Home, Pantalla 13" Pulgadas, 16 GB Intel Optane, SSD 256GB, Intel Core I5 - 10210U 1.6 GHz, RAM 8 GB DDR4, Garantía 12 Meses.</p>                
             </div>
             <div class="card-footer bg-light text-center">
-                <a href="#" class="btn btn-primary btn-sm my-1">Ver Producto</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalProducto">Ver Producto</button>
             </div>
         </div>
 
@@ -88,16 +89,16 @@
                 <img class="card-img-top img-fluid" src="https://i.postimg.cc/63tw5s1z/Portatil-4.jpg" alt="Card image">
             </a>
             <div class="card-body bg-light border shadow-sm p-2">
-                <h6 class="card-title text-center">
+                <h4 class="card-title text-center">
                    <strong>$ 2.399.000 COP</strong>
-                </h6>
+                </h4>
                 <div class="clearfix text-center">
                     <kbd class="">Descripci&oacute;n</kbd>
                 </div>
                 <p class="card-text text-justify small mt-2">Portátil ASUS X509JA-BR356T, Sistema Operativo Windows 10 Home 64bit, Pantalla: 15.6" Pulgadas, SSD 256GB, Intel® Core™ I7-1065G7 Processor 1.3 GHz, RAM 4 GB DDR4, Garantía 12 Meses.</p>                
             </div>
             <div class="card-footer bg-light text-center">
-                <a href="#" class="btn btn-primary btn-sm my-1">Ver Producto</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalProducto">Ver Producto</button>
             </div>
         </div>
 
@@ -106,16 +107,16 @@
                 <img class="card-img-top img-fluid" src="https://i.postimg.cc/mgP7VJjd/Portatil-6.jpg" alt="Card image">
             </a>
             <div class="card-body bg-light border shadow-sm p-2">
-                <h6 class="card-title text-center">
+                <h4 class="card-title text-center">
                    <strong>$ 2.159.000 COP</strong>
-                </h6>
+                </h4>
                 <div class="clearfix text-center">
                     <kbd class="">Descripci&oacute;n</kbd>
                 </div>
                 <p class="card-text text-justify small mt-2">Portátil ACER A315-56-349T, Sistema Operativo Windows 10, Pantalla 15.6" Pulgadas, Disco Hibrido 1TB + SSD 128GB,  Intel Core i3 1005G1 1.20 GHz, RAM 8 GB DDR4, Garantía 12 Meses.</p>                
             </div>
             <div class="card-footer bg-light text-center">
-                <a href="#" class="btn btn-primary btn-sm my-1">Ver Producto</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalProducto">Ver Producto</button>
             </div>
         </div>        
     </div> 
@@ -125,16 +126,16 @@
                 <img class="card-img-top img-fluid" src="https://i.postimg.cc/pTLWJbYr/Escritorio-1.jpg" alt="Card image">
             </a>
             <div class="card-body bg-light border shadow-sm text-center p-2">
-                <h6 class="card-title text-center">
+                <h4 class="card-title text-center">
                    <strong>$ 1.799.000 COP</strong>
-                </h6>
+                </h4>
                 <div class="clearfix text-center">
                     <kbd class="">Descripci&oacute;n</kbd>
                 </div>
                 <p class="card-text text-justify small mt-2">Computador Escritorio All In One HP 22-DD0005LA, Sistema Operativo Windows 10 home 64, Pantalla 22" Pulgadas, HHDD 1TB, Procesador Intel Core i3-1005G1 1.2 Ghz, RAM 4 GB DDR4, Garantía 12 Meses.</p>
             </div>
             <div class="card-footer bg-light text-center">
-                <a href="#" class="btn btn-primary btn-sm my-1">Ver Producto</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalProducto">Ver Producto</button>
             </div>
         </div>
         <div class="card col-5th border-0 bg-transparent">
@@ -142,16 +143,16 @@
                 <img class="card-img-top img-fluid" src="https://i.postimg.cc/nr7hCKGG/Escritorio-2.jpg" alt="Card image">
             </a>
             <div class="card-body bg-light border shadow-sm text-center p-2">
-                <h6 class="card-title text-center">
+                <h4 class="card-title text-center">
                    <strong>$ 2.399.000 COP</strong>
-                </h6>
+                </h4>
                 <div class="clearfix text-center">
                     <kbd class="">Descripci&oacute;n</kbd>
                 </div>
                 <p class="card-text text-justify small mt-2">Computador Escritorio All In One LENOVO A340, Sistema Operativo: Windows 10 HOME 64 EM, Pantalla 23,8" Pulgadas, Disco Híbrido: 1TB HDD + SSD 128GB, Procesador Intel Core I3 10110U 2,1Ghz, RAM 4 GB DDR4, Garantía 12 Meses.</p>
             </div>
             <div class="card-footer bg-light text-center">
-                <a href="#" class="btn btn-primary btn-sm my-1">Ver Producto</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalProducto">Ver Producto</button>
             </div>
         </div>      
         <div class="card col-5th border-0 bg-transparent">
@@ -159,16 +160,16 @@
                 <img class="card-img-top img-fluid" src="https://i.postimg.cc/fLdNKzSw/Escritorio-3.jpg" alt="Card image">
             </a>
             <div class="card-body bg-light border shadow-sm text-center p-2">
-                <h6 class="card-title text-center">
+                <h4 class="card-title text-center">
                    <strong>$ 4.469.000 COP</strong>
-                </h6>
+                </h4>
                 <div class="clearfix text-center">
                     <kbd class="">Descripci&oacute;n</kbd>
                 </div>
                 <p class="card-text text-justify small mt-2">Computador Escritorio All in one HP Pavilion 24-XA113LA, Sistema Operativo Windows 10 HOME, Pantalla 24" Pulgadas, Disco Híbrido 1TB HDD + SSD 256GB, Procesador AMD Ryzen 7 3750H 2.3 Ghz, RAM 8 GB DDR4, Garantía 12 Meses.</p>
             </div>
             <div class="card-footer bg-light text-center">
-                <a href="#" class="btn btn-primary btn-sm my-1">Ver Producto</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalProducto">Ver Producto</button>
             </div>
         </div>
         <div class="card col-5th border-0 bg-transparent">
@@ -176,16 +177,16 @@
                 <img class="card-img-top img-fluid" src="https://i.postimg.cc/8C28s2Kx/Escritorio-4.jpg" alt="Card image">
             </a>
             <div class="card-body bg-light border shadow-sm text-center p-2">
-                <h6 class="card-title text-center">
+                <h4 class="card-title text-center">
                    <strong>$ 2.349.000 COP</strong>
-                </h6>
+                </h4>
                 <div class="clearfix text-center">
                     <kbd class="">Descripci&oacute;n</kbd>
                 </div>
                 <p class="card-text text-justify small mt-2">Computador Escritorio All In One ASUS Vivo AIO M241DAK-WA113T, Sistema Operativo Windows 10 HOME, Pantalla 23,8" Pulgadas, HHDD 1TB, AMD Ryzen 5 3500U Processor 2.1 GHz, RAM 4 GB DDR4, Garantía 12 Meses.</p>
             </div>
             <div class="card-footer bg-light text-center">
-                <a href="#" class="btn btn-primary btn-sm my-1">Ver Producto</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalProducto">Ver Producto</button>
             </div>
         </div>  
         <div class="card col-5th border-0 bg-transparent">
@@ -193,17 +194,56 @@
                 <img class="card-img-top img-fluid" src="https://i.postimg.cc/8kB8Pnhn/Escritorio-5.jpg" alt="Card image">
             </a>
             <div class="card-body bg-light border shadow-sm text-center p-2">
-                <h6 class="card-title text-center">
+                <h4 class="card-title text-center">
                    <strong>$ 3.799.000 COP</strong>
-                </h6>
+                </h4>
                 <div class="clearfix text-center">
                     <kbd class="">Descripci&oacute;n</kbd>
                 </div>
                 <p class="card-text text-justify small mt-2">Computador Escritorio All in one HP Pavilion 24-XA002LA, Sistema Operativo Windows 10 HOME, Pantalla 24" Pulgadas, HHDD 1TB,  Intel Core i7-8700T 2.4 Ghz, RAM 4 GB DDR4, Garantía 12 Meses.</p>
             </div>
             <div class="card-footer bg-light text-center">
-                <a href="#" class="btn btn-primary btn-sm my-1">Ver Producto</a>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalProducto">Ver Producto</button>
             </div>
         </div>                   
-    </div>     
+    </div>
+    <div class="modal fade" tab-index="-1" id="modalProducto" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content px-3 pb-3">
+                <div class="mr-2 mt-2">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-9" id="imgModal">
+                        <img class="img-fluid" src="https://i.postimg.cc/8kB8Pnhn/Escritorio-5.jpg" alt="">
+                    </div>
+                </div>                
+                <div class="row d-flex justify-content-center">
+                    <div class="col-9 text-justify border p-3 mx-2 bg-light">
+                        <div class="row">
+                            <div class="col text-center">
+                                <h4>
+                                    <strong>$ 4.469.000</strong>
+                                </h4>
+                            </div>
+                        </div>
+                        <div class="row my-2">
+                            <div class="col text-center">
+                                <kbd class="px-2">Descripción</kbd>
+                            </div>
+                        </div>
+                        <p>Computador Escritorio All in one HP Pavilion 24-XA002LA, Sistema Operativo Windows 10 HOME, Pantalla 24" Pulgadas, HHDD 1TB,  Intel Core i7-8700T 2.4 Ghz, RAM 4 GB DDR4, Garantía 12 Meses.</p>                        
+                    </div>
+                </div>
+                <div class="row my-3">
+                    <div class="col text-center">
+                        <a href="modificar" class="btn btn-warning mr-3">Modificar</a>
+                        <a href="#" class="btn btn-danger">Eliminar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>          
 </div>
